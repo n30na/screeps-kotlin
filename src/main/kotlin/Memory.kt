0b66b4164f9f03418f50ada9fa1443f53c02cbee
neona <n30naa@gmail.com>
2019-01-16 10:32:39 -0800

    block out initial code structure

---
+++ src/main/kotlin/Memory.kt
@@ -0,0 +1,11 @@
+/**
+ * Generic memory storage delegate, uses [DECIDE ON SOMETHING] as the class storage key
+ */
+class MemoryDelegate {
+    operator fun getValue() {
+
+    }
+    operator fun setValue() {
+
+    }
+}
